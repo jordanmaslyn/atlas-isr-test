1621c6e77aed7d103a9e8f5ec4251b2c0f416f88
Jordan Maslyn <jordan.maslyn@wpengine.com>
2024-01-24 14:47:01 -0500

    Refine the check for build vs regeneration

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import { getWordPressProps, WordPressTemplate } from "@faustwp/core";
-import { PHASE_PRODUCTION_SERVER } from "next/constants";
+import { PHASE_PRODUCTION_BUILD } from "next/constants";
 
 export default function Page(props) {
   return <WordPressTemplate {...props} />;
 }
 
 export async function getStaticProps(ctx) {
-  console.log(`Next Phase is: ${process.env.NEXT_PHASE}`);
-  if (process.env.NEXT_PHASE === PHASE_PRODUCTION_SERVER) {
+  if (
+    process.env.ATLAS_METADATA_ENV_NAME !== undefined &&
+    process.env.NEXT_PHASE !== PHASE_PRODUCTION_BUILD
+  ) {
     await new Promise((r) => setTimeout(r, 60));
   }
+
   return getWordPressProps({ ctx, revalidate: 5 });
 }
