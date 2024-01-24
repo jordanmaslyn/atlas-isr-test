074e75a923f5d297c8de92c2dd3655afa98b93be
Jordan Maslyn <jordan.maslyn@wpengine.com>
2024-01-24 14:39:57 -0500

    Try to check for revalidation before sleeping

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { getWordPressProps, WordPressTemplate } from "@faustwp/core";
+import { PHASE_PRODUCTION_SERVER } from "next/constants";
 
 export default function Page(props) {
   return <WordPressTemplate {...props} />;
 }
 
 export async function getStaticProps(ctx) {
-  await new Promise((r) => setTimeout(r, 60));
+  console.log(`Next Phase is: ${process.env.NEXT_PHASE}`);
+  if (process.env.NEXT_PHASE === PHASE_PRODUCTION_SERVER) {
+    await new Promise((r) => setTimeout(r, 60));
+  }
   return getWordPressProps({ ctx, revalidate: 5 });
 }
