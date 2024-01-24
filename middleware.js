b3347ea3e83bb01f223b545b82072c19583362f9
Jordan Maslyn <jordan.maslyn@wpengine.com>
2024-01-24 17:07:08 -0500

    Add middleware to specify durations for SWR and SIE

---
+++ middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
+
+export async function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+
+  // You can also set request headers in NextResponse.rewrite
+  const response = NextResponse.next({
+    request: {
+      // New request headers
+      headers: requestHeaders,
+    },
+  });
+
+  // Set a new response header `cdn-cache-control`
+  response.headers.set(
+    "cdn-cache-control",
+    "stale-while-revalidate=600 stale-if-error=600"
+  );
+  return response;
+}
